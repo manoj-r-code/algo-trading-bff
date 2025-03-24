72a9788b65de1dfb7588c44682a89db3ccf14014
manoj-r-code <manoj.ravindra.123@gmail.com>
2025-03-24 17:29:19 +0530

    added deployed url for CORS

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:4200"," https://algo-trading-ffb.netlify.app"])  
+CORS(app, origins=["http://localhost:4200","https://algo-trading-ffb.netlify.app"])  
 
 
 @app.route('/')
